0a20a95efd87815b1329ffdca003412806c3ba8e
Martine Dowden <martine@dowden.us>
2020-06-04 01:52:11 -0400

    add fields

--- src/components/payment-form.vue
+++ src/components/payment-form.vue
@@ -5,11 +5,13 @@
       Need a form with the following fields:
       - First Name, required, max length 100
       - Last Name, required, max length 100
+      - Email, required, max length 100
       - Drop down, required, with the following options:
         * Visa
         * Mastercard
         * American Express
       - Card Number, required, max length 100
+      - Expiration date, required
     </pre>
   </div>
 </template>
