de3e2819fda4e4d6645f89138307be4e74aa1fdc
Michael Dowden <michael@dowden.us>
2020-06-22 15:00:28 -0400

    fixed label

--- src/components/products.vue
+++ src/components/products.vue
@@ -3,7 +3,7 @@
     <h1>Products: Toy Cars</h1>
     <ul>
       <li v-for="product in products" :key="product.id">
-        <h2 :id="'h2_'+product.id">{{product.year }} {{ product.make }} {{ product.model }}</h2>
+        <!-- <h2 :id="'h2_'+product.id">{{product.year }} {{ product.make }} {{ product.model }}</h2> -->
         <dl :id="'dl_'+product.id">
           <dt>Make:</dt> <dd>{{ product.make }} </dd>
           <dt>Model:</dt> <dd>{{ product.model }} </dd>
@@ -14,7 +14,8 @@
         <button 
           type="button"
           aria-controls="cart"
-          :aria-labelledby="'h2_'+product.id"
+          :aria-label="product.year+' '+product.make+' '+product.model"
+          
           :aria-details="'dl_'+product.id"
         >Buy Now</button>
       </li>
